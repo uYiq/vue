5de29a53b1d96fa5f0d38be4d46f8e12cbebd527
uYiq <141986010+uYiq@users.noreply.github.com>
2023-08-15 17:31:48 +0800

    Update HelloWorld.vue
    0815下班修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,7 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <h1>haha</h1>
   </div>
 </template>
 
